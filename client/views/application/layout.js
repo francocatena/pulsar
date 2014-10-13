dba56df22d589149cc54865d747ac2e8fcd57ce7
Franco Catena <francocatena@gmail.com>
2014-10-13 19:19:41 -0300

    Remove duplication in layout.js

--- client/views/application/layout.js
+++ client/views/application/layout.js
@@ -3,7 +3,7 @@ Template.layout.rendered = function () {
     if (Router.current() && Router.current().route.name != 'slide') return
 
     var key       = event.which
-    var currentId = Router.current() && Router.current().data()._id
+    var currentId = Router.current() && Router.current().params._id
     var nextKeys  = [39, 34, 40, 13]
     var prevKeys  = [37, 33, 38, 8]
 
@@ -12,8 +12,8 @@ Template.layout.rendered = function () {
     else if (_.contains(prevKeys, key))
       Meteor.call('changeSlide', currentId, 'prev')
     else if (_.contains([78, 110], key) && event.ctrlKey && event.altKey)
-      Router.go('slideNew', { _id: Router.current().params._id })
+      Router.go('slideNew',  { _id: currentId })
     else if (_.contains([69, 101], key) && event.ctrlKey && event.altKey)
-      Router.go('slideEdit', { _id: Router.current().params._id })
+      Router.go('slideEdit', { _id: currentId })
   })
 }

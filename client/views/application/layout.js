57d43bbac3d9d1752f0559427ae1d5ff432b0c7e
Franco Catena <francocatena@gmail.com>
2015-05-21 19:08:01 -0300

    Use click as next (mostly for mobile)

--- client/views/application/layout.js
+++ client/views/application/layout.js
@@ -1,21 +1,23 @@
 Template.layout.onCreated(function () {
-  $(document).on('keyup', function (event) {
+  $(document).on('keyup click', function (event) {
     var router   = Router.current()
     var slideId  = router && router.params._id
-    var key      = event.which
-    var nextKeys = [39, 34, 40, 13]
-    var prevKeys = [37, 33, 38, 8]
+    var which    = event.which
+    var type     = event.type
 
-    if (! Meteor.user()) return
+    var keyboardNext = _.contains([39, 34, 40, 13], which)
+    var keyboardPrev = _.contains([37, 33, 38, 8],  which)
+    var mouseNext    = type === 'click'
+
     if (router && router.route.getName() !== 'slide') return
 
-    if (_.contains(nextKeys, key))
+    if (keyboardNext || mouseNext)
       Meteor.call('changeSlide', slideId, 'next')
-    else if (_.contains(prevKeys, key))
+    else if (keyboardPrev)
       Meteor.call('changeSlide', slideId, 'prev')
-    else if (_.contains([78, 110], key) && event.ctrlKey && event.altKey)
+    else if (_.contains([78, 110], which) && event.ctrlKey && event.altKey)
       Router.go('slideNew',  { _id: slideId })
-    else if (_.contains([69, 101], key) && event.ctrlKey && event.altKey)
+    else if (_.contains([69, 101], which) && event.ctrlKey && event.altKey)
       Router.go('slideEdit', { _id: slideId })
   })
 })

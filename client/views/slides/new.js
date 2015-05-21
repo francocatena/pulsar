deb985e2cab6b8909dbe2137aa65426264ff1ac7
Franco Catena <francocatena@gmail.com>
2015-05-21 15:39:40 -0300

    Better next slide number handling...

--- client/views/slides/new.js
+++ client/views/slides/new.js
@@ -1,6 +1,6 @@
 Template.slideNew.helpers({
   nextNumber: function () {
-    return ++Slides.findOne({}, { sort: { number: -1 } }).number
+    return this.lastSlide ? ++this.lastSlide.number : 1
   }
 })
 
